db0687c874a6a941ea4a86978ec351ec3b5dfebd
Thiago dos Santos <thiagots_outlook.com>
2018-09-30 22:41:06 -0300

    Validações prontas

--- services/perguntaService.js
+++ services/perguntaService.js
@@ -2,13 +2,68 @@ class perguntaService{
     constructor(){}
 
     JsonGet(){
-
+        for(var i=0; i < data.length; i++){
+            data[i].respostas = JSON.parse(data[i].respostas); 
+        } 
+        return data;
+      }
     }
-    validarPerguntas(){
+    validarPerguntas(data){
+        var teste = 0 ;
+        var alternativas = ['a','b','c','d','e'];
+
+       
         
+        // Verificar se existe letras duplicadas ou sem ordem
+        for (var i=0; i<5;i++){
+            if(data.respostas[i].label != alternativas[i]){
+                return {status: false , message: 'A ordem das alternativas estão incorretas ou duplicadas'};
+            }
+        }
+         //Verificar se a descrição estava vazia
+         for (var i=0; i<5;i++){
+            if(data.respostas[i].descricao = ''){
+                teste++;
+            }if(teste>=1){
+                return {status: false , message: "Descrição esta vazia"};
+            }
+        }
+
+         //Verificar se a condição estava vazia
+         for (var i=0; i<5;i++){
+             teste=0;
+            if(data.respostas[i].condicao = ''){
+                teste++;
+            }if(teste>=1){
+                return {status: false , message: "A condição esta vazia"};
+            };
+        }
+
+        //Verificar se está true or false
+        for (var i=0; i<5;i++){
+           if(data.respostas[i].condicao != 'true' || data.respostas[i].condicao != 'false' ){
+               return {status: false , message: "A condição deve ser true ou false" };
+            }
+        }
+
+        //Verificar se existe mais de uma resposta true
+        for (var i=0; i<5;i++){
+            teste = 0;
+            if(data.respostas[i].condicao = 'true'){
+                teste++;
+            }if(teste>1){
+                return {status: false , message: "Só pode existir uma resposta verdadeira"};
+            }if(i=4){
+                if(teste = 0){
+                    return {status: false , message: "Deve existir pelo menos uma resposta verdadeira"};
+                }
+            }
+        }
+        
+        return {status: true};
     }
 
-}
+
 module.exports =  function(){
     return perguntaService;
 }
