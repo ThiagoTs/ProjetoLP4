63803f7519e511c6dee190b8dda5f2d5913d9840
Thiago dos Santos <thiagots_outlook.com>
2018-10-03 01:05:15 -0300

    Descrição não salva

--- controllers/pergunta.js
+++ controllers/pergunta.js
@@ -86,63 +86,59 @@ module.exports = function(app){
     /** POST /pergunta 
      *  rota que permite criar uma nova pergunta
     */
-    app.post('/pergunta', function(req, resp){
-        /**Propriedade que permite manipular o body da requisição */
-        var data = req.body;
-    
-        /**objetos quer me permite conectar no banco e maninpular as operações */
-        var con = app.persistencia.connectionFactory;
-        var dao = new app.persistencia.perguntaDAO(con);
-    
-        /**
-         * valida se os dados estão corretos conforme regra de negócio
-         */
-        var service = new app.services.perguntaService();
-        response = service.validarPerguntas(data);
-        if(!response.status){
-            resp.status(400);
-            resp.json({"message": response.message});   
-          return;
-        }else{
-            data.respostas = JSON.stringify(data.respostas);
-        }
-    
-        /**função assíncrona, como não sabemos quanto tempo irá demorar a conexão com banco. 
-         * É importante que as operações sejam assíncronas. O quer dizer que a função create
-         * será realizada, porém não será aguardado um retorno, as rotinas irão continuar sem 
-         * um retorno de create, por isto tempos a função anônima passada no segundo parâmetro 
-         * que é responsável por manipular o retorno da fução assíncrona. 
-         */
-        data.deletado = 0;
-        dao.create(data, function(exception, result){
-            if(exception){
-    
-                /**
-                 * verifica se a exceção é de chave única duplicada, no qual email se enquadra 
-                 * na exceção
-                 */
-                if(exception.code === 'ER_DUP_ENTRY'){
-                    resp.status(400);
-                    resp.send({"mensagem":"Pergunta já cadastrada"});
-                    return;
-                }
-                /**
-                 * error genéricos
-                 */
-                resp.status(500);
-                resp.send({"mensagem":"erro ao salvar a pergunta"});
-                console.log(exception);
+   app.post('/pergunta', function(req, resp){
+    /**Propriedade que permite manipular o body da requisição */
+    var  data = req.body;
+
+    /**objetos quer me permite conectar no banco e maninpular as operações */
+    var con = app.persistencia.connectionFactory;
+    var dao = new app.persistencia.perguntaDAO(con);
+
+    /**
+     * valida se os dados estão corretos conforme regra de negócio
+     */
+    var service = new app.services.perguntaService();
+    response = service.validarPerguntas(data);
+    if(!response.status){
+        resp.status(400);
+        resp.json({"message": response.message});   
+      return;
+    }else{
+        data.respostas = JSON.stringify(data.respostas);
+    }
+
+   
+    data.deletado = 0;
+    //data.respostas = JSON.stringify(data.respostas);
+    dao.create(data, function(exception, result){
+       
+        if(exception){
+
+            /**
+             * verifica se a exceção é de chave única duplicada, no qual email se enquadra 
+             * na exceção
+             */
+            if(exception.code === 'ER_DUP_ENTRY'){
+                resp.status(400);
+                resp.send({"mensagem":"pergunta já cadastrada"});
                 return;
             }
-    
             /**
-             * sucesso no cadastro
+             * error genéricos
              */
-           resp.status(201);
-           resp.send(data);
+            resp.status(500);
+            resp.send({"mensagem":"erro ao salvar pergunta"});
+            console.log(exception);
+            return;
+        }
+
+        /**
+         * sucesso no cadastro
+         */
+         resp.status(201);
+         resp.send(data);
         });
     });
-
     /** PUT /pergunta 
      *  rota que permite alterar uma pergunta pelo id
     */
